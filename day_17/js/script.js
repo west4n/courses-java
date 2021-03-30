7977700d7bf53414bc081e8c58166b38b48634ed
Ivan Kurski <iskurskiii@gmail.com>
2021-03-31 00:02:39 +0300

    added day 16,17

---
+++ day_17/js/script.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const box = document.getElementById('box');
+const btns = document.getElementsByTagName('button');
+const circles = document.getElementsByClassName('circle');
+const hearts = document.querySelectorAll('.heart');
+const oneHeart = document.querySelector('.heart');
