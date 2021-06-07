05ac93662d2d42992ef705399484177d325806a4
Ivan Kurski <iskurskiii@gmail.com>
2021-06-07 21:59:37 +0300

    add day 24; edit practic 2

---
+++ day_24/script.js
@@ -0,0 +1,26 @@
+'use strict';
+
+// const now = new Date();
+//
+// console.log(now.setHours(18));
+// console.log(now);
+
+// console.log(now);
+// console.log(now.getFullYear());
+// console.log(now.getMonth());
+// console.log(now.getDate());
+// console.log(now.getDay());
+// console.log(now.getUTCHours());
+
+// console.log(now.getTimezoneOffset());
+// console.log(now.getTime());
+
+let start = new Date();
+
+for (let i = 0; i < 100000; i++) {
+    let some = i ** 3;
+}
+
+let end = new Date();
+
+alert(`Цикл отработал за ${end - start} миллисекунд`);
